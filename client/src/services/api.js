08fa96696bfcddc2ef9fa78f9ab10347aab2b8ee
UmangSachdeva <umang965454@gmail.com>
2023-11-27 19:58:56 +0530

    Added cors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:8000';
+const API_URL = "https://file-sharing-mern.vercel.app";
 
 export const uploadFile = async (data) => {
-    try {
-        let response = await axios.post(`${API_URL}/upload`, data);
-        return response.data;
-    } catch (error) {
-        console.error('Error while calling the api', error.message);
-    }
-}
+  try {
+    let response = await axios.post(`${API_URL}/upload`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error while calling the api", error.message);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import router from "./routes/routes.js";
-import cors from 'cors';
+import cors from "cors";
 import DBConnection from "./database/db.js";
 
 const app = express();
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/", router);
 
 const PORT = 8000;
